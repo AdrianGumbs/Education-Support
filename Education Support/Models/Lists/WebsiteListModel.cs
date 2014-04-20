9f9cc49bafed2b211d0b85691faccd56c6f7bf35
adrian gumbs <ad_86@hotmail.co.uk>
2014-04-20 23:57:59 +0100

    website list model

---
+++ Education Support/Models/Lists/WebsiteListModel.cs
@@ -0,0 +1,32 @@
+﻿using Framework.Domain;
+using Framework.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wensite.Models.Lists
+{
+    public class WebsiteListModel : WebSite
+    {
+        public virtual IList<WebSite> WebsiteList { get; set; }
+
+        public WebsiteListModel(IWebsiteRepo _websiteRepo, string search = null)
+        {
+            this.WebsiteList = _websiteRepo.LoadAll();
+            if (!string.IsNullOrEmpty(search))
+            {
+                this.WebsiteList = Search(this.WebsiteList, search);
+            }
+        }
+        public IList<WebSite> Search(IList<WebSite> results, string search)
+        {
+            var _query = from m in results
+                         select m;
+            results = _query.Where(
+                                    m => m.Url.ToUpper().Contains(search.ToUpper()) ||
+                                    m.HostedBy.ToUpper().Contains(search.ToUpper()) ||
+                                    m.Authority.Name.ToUpper().Contains(search.ToUpper())
+                ).ToList<WebSite>();
+            return results;
+        }
+    }
+}
